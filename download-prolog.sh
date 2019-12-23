c993e73656d4c0805e1c2f1773e6f0956fcedfbe
Benjamin Rosas <ben@aliencyb.org>
2019-12-23 08:40:42 -0600

    add a few scripts

---
+++ download-prolog.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+exercism download --track=prolog --exercise=anagram
+exercism download --track=prolog --exercise=binary
+exercism download --track=prolog --exercise=complex-numbers
+exercism download --track=prolog --exercise=dominoes
+exercism download --track=prolog --exercise=grains
+exercism download --track=prolog --exercise=hamming
+exercism download --track=prolog --exercise=hello-world
+exercism download --track=prolog --exercise=isogram
+exercism download --track=prolog --exercise=leap
+exercism download --track=prolog --exercise=nucleotide-count
+exercism download --track=prolog --exercise=pascals-triangle
+exercism download --track=prolog --exercise=queen-attack
+exercism download --track=prolog --exercise=rna-transcription
+exercism download --track=prolog --exercise=satellite
+exercism download --track=prolog --exercise=space-age
+exercism download --track=prolog --exercise=spiral-matrix
+exercism download --track=prolog --exercise=sum-of-multiples
+exercism download --track=prolog --exercise=triangle
