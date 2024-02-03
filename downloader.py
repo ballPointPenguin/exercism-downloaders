decd86057132afd99b270ad22e507495da1d719f
Bennie Rosas <ben@aliencyb.org>
2024-02-03 09:36:05 -0600

    dont re-download existing ones

--- downloader.py
+++ downloader.py
@@ -2,16 +2,16 @@ import os
 import json
 import subprocess
 
-def download_exercises(json_directory):
-    for filename in os.listdir(json_directory):
+def download_exercises(json_dir, exercism_dir):
+    for filename in os.listdir(json_dir):
         if filename.endswith('.json'):
             track = filename.split('.')[0]
-            filepath = os.path.join(json_directory, filename)
+            filepath = os.path.join(json_dir, filename)
 
             with open(filepath, 'r') as file:
                 file_content = file.read()
                 print(f'Processing {filename}...')
-                # print(file_content)
+
                 try:
                     exercises = json.loads(file_content)
                 except json.JSONDecodeError as e:
@@ -19,9 +19,12 @@ def download_exercises(json_directory):
                     continue # Skip to the next file
 
                 for exercise, status in exercises.items():
-                    if status[0] != 'locked':
+                    exercise_dir = os.path.join(exercism_dir, track, exercise)
+                    if status[0] != 'locked' and not os.path.exists(exercise_dir):
                         command = f'exercism download --exercise={exercise} --track={track}'
                         subprocess.run(command, shell=True)
 
-json_directory = 'json'  # Path to the directory containing JSON files
-download_exercises(json_directory)
+json_dir = 'json'  # Local path to the directory containing JSON files
+exercism_dir = os.path.expanduser('~/exercism')
+
+download_exercises(json_dir, exercism_dir)
