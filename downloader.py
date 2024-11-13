a4c46ae7f50688da9a0999cf20ecd9c103ac3cf6
Bennie Rosas <bennie.rosas@blvd.co>
2024-11-12 23:58:17 -0600

    allow --force flag

--- downloader.py
+++ downloader.py
@@ -1,9 +1,13 @@
+"""This module provides functionality to download Exercism exercises for one or more tracks."""
+
 import os
 import json
 import subprocess
 import argparse
 
-def download_exercises(json_dir, exercism_dir, track=None):
+
+def download_exercises(json_dir, exercism_dir, track=None, force=False):
+    """Download Exercism exercises from JSON configuration files."""
     if track:
         filenames = [f'{track}.json']
     else:
@@ -14,7 +18,7 @@ def download_exercises(json_dir, exercism_dir, track=None):
             track = filename.split('.')[0]
             filepath = os.path.join(json_dir, filename)
 
-            with open(filepath, 'r') as file:
+            with open(filepath, 'r', encoding='utf-8') as file:
                 file_content = file.read()
                 print(f'Processing {filename}...')
 
@@ -25,20 +29,19 @@ def download_exercises(json_dir, exercism_dir, track=None):
                     continue # Skip to the next file
 
                 for exercise, status in exercises.items():
-                    # log exercise
                     exercise_dir = os.path.join(exercism_dir, track, exercise)
-                    if status[0] != 'locked' and not os.path.exists(exercise_dir):
-                        # log exercise_dir
+                    if status[0] != 'locked' and (force or not os.path.exists(exercise_dir)):
                         print(f"Downloading {exercise}...")
-                        command = f'exercism download --exercise={exercise} --track={track}'
-                        subprocess.run(command, shell=True)
-
+                        force_flag = '--force' if force else ''
+                        command = f'exercism download --exercise={exercise} --track={track} {force_flag}'
+                        subprocess.run(command, shell=True, check=True)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Download exercism exercises')
     parser.add_argument('--track', type=str, help='Track to download exercises for')
+    parser.add_argument('--force', action='store_true', help='Force download even if exercise already exists')
     args = parser.parse_args()
 
-    json_dir = 'json'  # Local path to the directory containing JSON files
-    exercism_dir = os.path.expanduser('~/exercism')
+    JSON_DIR = 'json'  # Local path to the directory containing JSON files
+    EXERCISM_DIR = os.path.expanduser('~/exercism')
 
-    download_exercises(json_dir, exercism_dir, track=args.track)
+    download_exercises(JSON_DIR, EXERCISM_DIR, track=args.track, force=args.force)
